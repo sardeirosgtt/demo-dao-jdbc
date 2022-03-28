1bd8bd51b5a597d507d346defe37d05b61b2b1cf
sardeirosgt <ddandre1010@gmail.com>
2022-03-27 23:43:29 -0300

    criando class seller

--- src/aplicação/Program.java
+++ src/aplicação/Program.java
@@ -1,6 +1,9 @@
 package aplicação;
 
+import java.util.Date;
+
 import model.entidades.Department;
+import model.entidades.Seller;
 
 public class Program {
 
@@ -9,7 +12,11 @@ public class Program {
 		
 		Department obj = new Department(1,"books");
 		
-		System.out.println(obj);
+		
+		
+		Seller seller = new Seller (21,"bob","ddandre1010@gmail.com", new Date(), 3000.00, obj);
+		
+		System.out.println(seller);
 
 	}
 
