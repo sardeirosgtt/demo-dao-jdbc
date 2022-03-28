23ae5a4f890ffaa554abc2f55333285b92463326
sardeirosgt <ddandre1010@gmail.com>
2022-03-27 23:27:03 -0300

    criando class department

---
+++ src/aplicação/Program.java
@@ -0,0 +1,16 @@
+package aplicação;
+
+import model.entidades.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		
+		Department obj = new Department(1,"books");
+		
+		System.out.println(obj);
+
+	}
+
+}
